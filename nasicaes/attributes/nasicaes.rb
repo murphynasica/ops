fb68db9117ac8a2ffc0690f759ab8e0f09d32db6
Bobby Richards <bobby.richards@gmail.com>
2015-01-13 13:49:32 -0600

    elasticsearch config in wrong place, fixed

--- nasicaes/attributes/nasicaes.rb
+++ nasicaes/attributes/nasicaes.rb
@@ -2,6 +2,6 @@ node.default['java']['jdk_version'] = "7"
 node.default['java']['oracle']['accept_oracle_download_terms'] = "true"
 node.default['elasticsearch']['version'] = "1.4.2"
 node.default['elasticsearch']['cluster']['name'] = "nasica"
+node.default['elasticsearch']['discovery']['type'] = "ec2"
 default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
-default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['discovery']['type'] = 'ec2'
 
