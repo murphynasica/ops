f110726a64e099fc6d81a74e187ed823527a4289
Bobby Richards <bobby.richards@gmail.com>
2015-01-12 19:12:08 -0600

    elasticsearch aws plugin version change

--- nasicaes/attributes/nasicaes.rb
+++ nasicaes/attributes/nasicaes.rb
@@ -1,5 +1,5 @@
 node.default['java']['oracle']['accept_oracle_download_terms'] = "true"
 node.default['elasticsearch']['version'] = "1.4.2"
 node.default['elasticsearch']['cluster']['name'] = "nasica"
-
+default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
 
