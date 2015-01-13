02a23c641b725c9f8d9d4c392a8c61db4bf8afdc
Bobby Richards <bobby.richards@gmail.com>
2015-01-12 19:04:49 -0600

    updating elasticsearch and java versions

--- Berksfile
+++ Berksfile
@@ -5,5 +5,5 @@ cookbook 'ark'
 cookbook 'apparmor'
 cookbook 'ntp'
 cookbook 'vim'
-cookbook 'java'
+cookbook 'java', ">=1.7"
 cookbook 'elasticsearch', github: "elasticsearch/cookbook-elasticsearch"
--- nasicaes/attributes/nasicaes.rb
+++ nasicaes/attributes/nasicaes.rb
@@ -1,4 +1,5 @@
 node.default['java']['oracle']['accept_oracle_download_terms'] = "true"
+node.default['elasticsearch']['version'] = "1.4.2"
 node.default['elasticsearch']['cluster']['name'] = "nasica"
 
 
