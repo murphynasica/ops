7f51c466e67ec28a21e0e4fd1b23fce99d23ab58
Bobby Richards <bobby.richards@gmail.com>
2015-01-12 20:56:59 -0600

    ec2 discovery attributes

--- nasicaes/attributes/nasicaes.rb
+++ nasicaes/attributes/nasicaes.rb
@@ -1,4 +1,5 @@
 node.default['elasticsearch']['version'] = "1.4.2"
 node.default['elasticsearch']['cluster']['name'] = "nasica"
 default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.4.1'
+default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['discovery']['type'] = 'ec2'
 
