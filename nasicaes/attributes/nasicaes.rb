edafe4e24d5dd9af222e0c5cca4f606fa7c031ce
Bobby Richards <bobby.richards@gmail.com>
2015-01-12 20:08:26 -0600

    java 7 in attributes file

--- nasicaes/attributes/nasicaes.rb
+++ nasicaes/attributes/nasicaes.rb
@@ -1,3 +1,4 @@
+node.default['java']['jdk_version'] = "7"
 node.default['java']['oracle']['accept_oracle_download_terms'] = "true"
 node.default['elasticsearch']['version'] = "1.4.2"
 node.default['elasticsearch']['cluster']['name'] = "nasica"
