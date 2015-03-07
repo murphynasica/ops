0d369ce69273b3b6accee6c22bbc4d7d4d9ae84c
Bobby Richards <bobby@fxtickr.com>
2015-03-06 23:51:58 -0600

    Update nasicaes.rb
    latest elasticsearch for kibana

--- nasicaes/attributes/nasicaes.rb
+++ nasicaes/attributes/nasicaes.rb
@@ -1,6 +1,6 @@
 node.default['java']['jdk_version'] = "7"
 node.default['java']['oracle']['accept_oracle_download_terms'] = "true"
-node.default['elasticsearch']['version'] = "1.4.2"
+node.default['elasticsearch']['version'] = "1.4.4"
 node.default['elasticsearch']['cluster']['name'] = "nasica"
 node.default['elasticsearch']['discovery']['type'] = "ec2"
 node.default['elasticsearch']['discovery']['zen']['ping']['multicast']['enabled'] = "false"
